fbf04f75332da84ec3cb7c2e8a948782e9138283
SweatyJujuNon <1ikbess@seattleschools.org>
2023-03-15 19:45:58 -0800

    Update

--- src/main/java/org/SweatyJujuNon/Launcher/Main.java
+++ src/main/java/org/SweatyJujuNon/Launcher/Main.java
@@ -18,7 +18,8 @@ public class Main {
 
     public static final Gson gson = new Gson();
 
-    public static void main(String[] args) throws NullPointerException,IOException {
+    public static void main(String[] args) throws NullPointerException, IOException, InterruptedException {
+        ArrayList<Thread> threads = new ArrayList<>();
         URL url = new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
@@ -45,11 +46,36 @@ public class Main {
         while ((line = reader.readLine()) != null) {
             response.append(line);
         }
+
+        try {
+            URLConnection connection1 = url.openConnection();
+            InputStream inputStream = connection1.getInputStream();
+
+            File file = new File(".minecraft/versions/"+v);
+            file.mkdirs();
+
+            // Open a FileOutputStream to save the file
+            FileOutputStream outputStream = new FileOutputStream(".minecraft/versions/"+v+"/"+v+".json");
+
+            // Read bytes from the input stream and write them to the output stream
+            byte[] buffer = new byte[1024];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            // Close the input and output streams
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
         String clientJarUrl = ((LinkedTreeMap)((LinkedTreeMap)gson.fromJson(response.toString(), LinkedTreeMap.class).get("downloads")).get("client")).get("url").toString();
 
         String assetsUrl = ((LinkedTreeMap)gson.fromJson(response.toString(), LinkedTreeMap.class).get("assetIndex")).get("url").toString();
         {
-            URL a = new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json");
+            URL a = new URL(assetsUrl);
             HttpURLConnection b = (HttpURLConnection) a.openConnection();
             b.setRequestMethod("GET");
             BufferedReader c = new BufferedReader(new InputStreamReader(b.getInputStream()));
@@ -59,33 +85,40 @@ public class Main {
                 d.append(e);
             }
 
+
             LinkedTreeMap<String,LinkedTreeMap> assets = (LinkedTreeMap) gson.fromJson(d.toString(), LinkedTreeMap.class).get("objects");
             for (Map.Entry<String,LinkedTreeMap> f : assets.entrySet()) {
-                LinkedTreeMap asset = f.getValue();
-                File folder = new File(".minecraft/assets/objects/"+asset.get("hash").toString().substring(0,2));
-                folder.mkdirs();
+                Thread thread = new Thread(() -> {
+                    LinkedTreeMap asset = f.getValue();
+                    File folder = new File(".minecraft/assets/objects/"+asset.get("hash").toString().substring(0,2));
+                    folder.mkdirs();
 
-                try {
-                    URL url1 = new URL("https://resources.download.minecraft.net/"+asset.get("hash").toString().substring(0,2)+"/"+asset.get("hash").toString());
-                    URLConnection connection1 = url1.openConnection();
-                    InputStream inputStream = connection1.getInputStream();
+                    try {
+                        URL url1 = new URL("https://resources.download.minecraft.net/"+asset.get("hash").toString().substring(0,2)+"/"+asset.get("hash").toString());
+                        URLConnection connection1 = url1.openConnection();
+                        InputStream inputStream = connection1.getInputStream();
 
-                    // Open a FileOutputStream to save the file
-                    FileOutputStream outputStream = new FileOutputStream(".minecraft/assets/objects/"+asset.get("hash").toString().substring(0,2)+"/"+asset.get("hash").toString());
+                        // Open a FileOutputStream to save the file
+                        FileOutputStream outputStream = new FileOutputStream(".minecraft/assets/objects/"+asset.get("hash").toString().substring(0,2)+"/"+asset.get("hash").toString());
 
-                    // Read bytes from the input stream and write them to the output stream
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = -1;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, bytesRead);
+                        // Read bytes from the input stream and write them to the output stream
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = -1;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                        }
+
+                        // Close the input and output streams
+                        inputStream.close();
+                        outputStream.close();
+                    } catch (IOException exception) {
+                        exception.printStackTrace();
                     }
+                });
 
-                    // Close the input and output streams
-                    inputStream.close();
-                    outputStream.close();
-                } catch (IOException exception) {
-                    exception.printStackTrace();
-                }
+                threads.add(thread);
+                thread.start();
+
             }
         }
 
@@ -135,14 +168,8 @@ public class Main {
             for (int i = 0; i < p.length - 1; i++) {
                 path = path + p[i] + "/";
             }
-            File file = new File(".minecraft/libraries/"+path);
-            if (!file.exists()) {
-                boolean success = file.mkdirs();
-                if (!success) {
-                    System.out.println("Failed to create directories");
-                }
-            }
-
+            File folder = new File(".minecraft/libraries/"+path);
+            folder.mkdirs();
             try {
                 URL a = new URL(u);
                 URLConnection b = a.openConnection();
@@ -169,7 +196,7 @@ public class Main {
 
 
         }
-        File folder = new File(".minecraft/version/"+v+"/");
+        File folder = new File(".minecraft/versions/"+v+"/");
         folder.mkdirs();
         try {
             URL a = new URL(clientJarUrl);
@@ -177,7 +204,7 @@ public class Main {
             InputStream inputStream = b.getInputStream();
 
             // Open a FileOutputStream to save the file
-            FileOutputStream outputStream = new FileOutputStream(".minecraft/version/"+v+"/" + v + ".jar");
+            FileOutputStream outputStream = new FileOutputStream(".minecraft/versions/"+v+"/" + v + ".jar");
 
             // Read bytes from the input stream and write them to the output stream
             byte[] buffer = new byte[1024];
@@ -194,7 +221,18 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for (Thread thread : threads) {
+            thread.join();
+        }
         reader.close();
         connection.disconnect();
+        ProcessBuilder builder = new ProcessBuilder("java","-Djava.library.path=C:/Users/Storm/IdeaProjects/Launcher/.minecraft/versions/1.8.9/natives","-cp","C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/mojang/netty/1.8.8/netty-1.8.8.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/oshi-project/oshi-core/1.1/oshi-core-1.1.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/java/dev/jna/jna/3.4.0/jna-3.4.0.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/java/dev/jna/platform/3.4.0/platform-3.4.0.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/ibm/icu/icu4j-core-mojang/51.2/icu4j-core-mojang-51.2.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/sf/jopt-simple/jopt-simple/4.6/jopt-simple-4.6.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/paulscode/codecjorbis/20101023/codecjorbis-20101023.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/paulscode/codecwav/20101023/codecwav-20101023.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/paulscode/libraryjavasound/20101123/libraryjavasound-20101123.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/paulscode/librarylwjglopenal/20100824/librarylwjglopenal-20100824.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/paulscode/soundsystem/20120107/soundsystem-20120107.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/io/netty/netty-all/4.0.23.Final/netty-all-4.0.23.Final.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/google/guava/guava/17.0/guava-17.0.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/commons-io/commons-io/2.4/commons-io-2.4.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/commons-codec/commons-codec/1.9/commons-codec-1.9.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/google/code/gson/gson/2.2.4/gson-2.2.4.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/mojang/authlib/1.5.21/authlib-1.5.21.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/com/mojang/realms/1.7.59/realms-1.7.59.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/apache/httpcomponents/httpcore/4.3.2/httpcore-4.3.2.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/apache/logging/log4j/log4j-api/2.0-beta9/log4j-api-2.0-beta9.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/apache/logging/log4j/log4j-core/2.0-beta9/log4j-core-2.0-beta9.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/lwjgl/lwjgl/lwjgl/2.9.4-nightly-20150209/lwjgl-2.9.4-nightly-20150209.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/lwjgl/lwjgl/lwjgl_util/2.9.4-nightly-20150209/lwjgl_util-2.9.4-nightly-20150209.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-windows.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/tv/twitch/twitch/6.5/twitch-6.5.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/tv/twitch/twitch-platform/6.5/twitch-platform-6.5.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/tv/twitch/twitch-platform/6.5/twitch-platform-6.5-natives-windows-64.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/tv/twitch/twitch-external-platform/4.5/twitch-external-platform-4.5.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/libraries/tv/twitch/twitch-external-platform/4.5/twitch-external-platform-4.5-natives-windows-64.jar;C:/Users/Storm/IdeaProjects/Launcher/.minecraft/versions/1.8.9/1.8.9.jar","net.minecraft.client.main.Main","--username","SweatyJujuNon","--version","1.8.9","--gameDir","C:/Users/Storm/IdeaProjects/Launcher/.minecraft","--assetsDir","C:/Users/Storm/IdeaProjects/Launcher/.minecraft/assets","--assetIndex","1.8","--uuid","f8f784d1-9b1a-45b1-a6a8-db1ac9a7011a","--accessToken","","--userProperties","{}","--userType","mojang");
+        Process process = builder.start();
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String l;
+        while ((l = r.readLine()) != null) {
+            System.out.println(l);
+        }
     }
 }
---
+++ src/main/java/org/SweatyJujuNon/Launcher/Utils.java
@@ -0,0 +1,121 @@
+package org.SweatyJujuNon.Launcher;
+
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class Utils {
+
+    public static final String TOKEN_URL = "https://login.microsoftonline.com/consumers/oauth2/v2.0/token";
+    public static final String AUTH_URL = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize/";
+    public static final String SCOPE = "XboxLive.signin offline_access";
+    public static final String CLIENT_ID = "41bf7724-369e-4223-996c-88e62ce3d151";
+    public static final String REDIRECT_URL = "http://localhost/auth-response";
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, ParseException {
+        String[] l = getSecureLoginData(CLIENT_ID,REDIRECT_URL,null);
+        String login_url = l[0];
+        String state = l[1];
+        String codeVerifier = l[2];
+
+        System.out.println(login_url);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter authorization code: ");
+        String authCode = scanner.nextLine();
+        System.out.println("Authorization code entered is: " + authCode);
+
+
+        System.out.println(getAuthorizationToken(CLIENT_ID,null,REDIRECT_URL,authCode,codeVerifier));
+    }
+
+    public static String[] getSecureLoginData(String clientId, String redirectUri, String state) throws NoSuchAlgorithmException {
+        String codeVerifier = generateCodeVerifier();
+        String codeChallenge = generateCodeChallenge(codeVerifier);
+        String codeChallengeMethod = "S256";
+
+        if (state == null) {
+            state = generateRandomState();
+        }
+
+        URI url = URI.create(AUTH_URL).resolve("?client_id=" + URLEncoder.encode(clientId, StandardCharsets.UTF_8)
+                + "&response_type=code"
+                + "&redirect_uri=" + URLEncoder.encode(redirectUri, StandardCharsets.UTF_8)
+                + "&response_mode=query"
+                + "&scope=" + URLEncoder.encode(SCOPE, StandardCharsets.UTF_8)
+                + "&state=" + URLEncoder.encode(state, StandardCharsets.UTF_8)
+                + "&code_challenge=" + URLEncoder.encode(codeChallenge, StandardCharsets.UTF_8)
+                + "&code_challenge_method=" + URLEncoder.encode(codeChallengeMethod, StandardCharsets.UTF_8));
+        String[] r = new String[3];
+        r[0] = url.toString();
+        r[1] = state;
+        r[2] = codeVerifier;
+        return r;
+    }
+
+    private static String generateRandomState() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[16];
+        random.nextBytes(bytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+
+    private static String generateCodeVerifier() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+
+    private static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = digest.digest(codeVerifier.getBytes(StandardCharsets.US_ASCII));
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+
+    public static String getAuthorizationToken(String clientId, String clientSecret, String redirectUri, String authCode, String codeVerifier) throws IOException, ParseException {
+        String parameters = "?client_id=" + URLEncoder.encode(clientId, StandardCharsets.UTF_8)
+                + "&scope=" + URLEncoder.encode("XboxLive.signin offline_access", StandardCharsets.UTF_8)
+                + "&code=" + URLEncoder.encode(authCode, StandardCharsets.UTF_8)
+                + "&redirect_uri=" + URLEncoder.encode(redirectUri, StandardCharsets.UTF_8)
+                + "&grant_type=authorization_code";
+
+        if (clientSecret != null) {
+            parameters += "&client_secret=" + URLEncoder.encode(clientSecret, StandardCharsets.UTF_8);
+        }
+
+        if (codeVerifier != null) {
+            parameters += "&code_verifier=" + URLEncoder.encode(codeVerifier, StandardCharsets.UTF_8);
+        }
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpPost httpPost = new HttpPost(TOKEN_URL+parameters);
+        httpPost.setHeader("Content-Type","application/x-www-form-urlencoded");
+        httpPost.setHeader("user-agent", "minecraft-launcher-lib/5.2");
+        httpPost.setHeader("Origin", "https://localhost");
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+
+        return response.toString();
+    }
+}
